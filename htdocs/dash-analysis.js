29f2780e3512a018a9528bb044c88660d3c75f00
cookie <axel.neergaard@gmail.com>
2020-11-19 20:31:44 +0200

    Add analytics capabilities to project

--- htdocs/dash-analysis.js
+++ htdocs/dash-analysis.js
@@ -1,46 +1,47 @@
+function setupAnalytics(video, player, analytics) {
+    analytics['metrics'] = {};
+    player.on(dashjs.MediaPlayer.events["PLAYBACK_ENDED"], function () {
+        clearInterval(eventPoller);
+    });
+
+    analytics['quality'] = {};
+    player.on('fragmentLoadingCompleted', function (value) {
+        if (value.mediaType === 'audio') {
+            return;
+        }
+        const timestamp = Date.now();
+        analytics['quality'][timestamp] = {
+            index: value.request.index,
+            quality: value.request.quality,
+        };
+    });
+
+    const eventPoller = setInterval(function () {
+        const streamInfo = player.getActiveStream().getStreamInfo();
+        const dashMetrics = player.getDashMetrics();
+        const dashAdapter = player.getDashAdapter();
+
+        if (dashMetrics && streamInfo) {
+            const periodIdx = streamInfo.index;
+
+            const bufferLevel = dashMetrics.getCurrentBufferLevel('video', true);
+
+            const repSwitch = dashMetrics.getCurrentRepresentationSwitch('video', true);
+            const bitrate = repSwitch ? Math.round(dashAdapter.getBandwidthForRepresentation(repSwitch.to, periodIdx) / 1000) : NaN;
+
+            const unixTimestamp = Date.now();
+            analytics['metrics'][unixTimestamp] = {
+                bufferLevel,
+                bitrate,
+            };
+        }
+    }, 1000);
+}
+
 function setupControls(player) {
     video.controls = false;
     const controlBar = new ControlBar(player);
     controlBar.initialize();
-}
-
-function setupAnalytics(video, player) {
-    player.on(dashjs.MediaPlayer.events["PLAYBACK_ENDED"], function () {
-        clearInterval(eventPoller);
-        clearInterval(bitrateCalculator);
-    });
-
-    var eventPoller = setInterval(function () {
-        var streamInfo = player.getActiveStream().getStreamInfo();
-        var dashMetrics = player.getDashMetrics();
-        var dashAdapter = player.getDashAdapter();
-
-        if (dashMetrics && streamInfo) {
-            const periodIdx = streamInfo.index;
-            var repSwitch = dashMetrics.getCurrentRepresentationSwitch('video', true);
-            var bufferLevel = dashMetrics.getCurrentBufferLevel('video', true);
-            var bitrate = repSwitch ? Math.round(dashAdapter.getBandwidthForRepresentation(repSwitch.to, periodIdx) / 1000) : NaN;
-            var adaptation = dashAdapter.getAdaptationForType(periodIdx, 'video', streamInfo)
-            var frameRate = adaptation.Representation_asArray.find(function (rep) {
-                return rep.id === repSwitch.to
-            }).frameRate;
-            document.getElementById('bufferLevel').innerText = bufferLevel + " secs";
-            document.getElementById('framerate').innerText = frameRate + " fps";
-            document.getElementById('reportedBitrate').innerText = bitrate + " Kbps";
-        }
-    }, 1000);
-
-    if (video.webkitVideoDecodedByteCount !== undefined) {
-        var lastDecodedByteCount = 0;
-        const bitrateInterval = 5;
-        var bitrateCalculator = setInterval(function () {
-            var calculatedBitrate = (((video.webkitVideoDecodedByteCount - lastDecodedByteCount) / 1000) * 8) / bitrateInterval;
-            document.getElementById('calculatedBitrate').innerText = Math.round(calculatedBitrate) + " Kbps";
-            lastDecodedByteCount = video.webkitVideoDecodedByteCount;
-        }, bitrateInterval * 1000);
-    } else {
-        //document.getElementById('chrome-only').style.display = "none";
-    }
 }
 
 const VIDEO_URL = '/videos/alazar/manifest.mpd';
@@ -54,4 +55,16 @@ player.updateSettings({
 });
 
 setupControls(player);
-setupAnalytics(video, player);
+
+const analytics = {};
+setupAnalytics(video, player, analytics);
+
+// https://stackoverflow.com/questions/34156282/how-do-i-save-json-to-local-text-file
+function downloadAnalytics() {
+    const tmp = document.createElement('a');
+    const file = new Blob([JSON.stringify(analytics)], { type: 'text/plain' });
+    tmp.href = URL.createObjectURL(file);
+    const timestamp = Date.now()
+    tmp.download = `multimedia_analytics_${timestamp}.json`;
+    tmp.click();
+}
