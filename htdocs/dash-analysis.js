211d021c3d7907a1d3d59c332152684e9b50b23b
cookie <axel.neergaard@gmail.com>
2020-11-20 23:21:34 +0200

    List all media player settings

--- htdocs/dash-analysis.js
+++ htdocs/dash-analysis.js
@@ -117,23 +117,110 @@ function applySettings(player) {
     const maxBitrate = parseInt(document.getElementById('maxBitrate').value, 10);
     const minBitrate = parseInt(document.getElementById('minBitrate').value, 10);
 
+    // http://cdn.dashjs.org/latest/jsdoc/module-Settings.html
     player.updateSettings({
-        debug: {
+         debug: {
             logLevel: dashjs.Debug.LOG_LEVEL_NONE,
-        },
-        'streaming': {
-            'stableBufferTime': stableBuffer,
-            'bufferTimeAtTopQualityLongForm': bufferAtTopQuality,
-            'abr': {
-                'minBitrate': {
-                    'video': minBitrate
-                },
-                'maxBitrate': {
-                    'video': maxBitrate
-                },
-            },
-            'scheduleWhilePaused': true,
-            'fastSwitchEnabled': false,
-        },
-    })
+         },
+         streaming: {
+             metricsMaxListDepth: 1000,
+             abandonLoadTimeout: 10000,
+             liveDelayFragmentCount: NaN,
+             liveDelay: null,
+             scheduleWhilePaused: true,
+             fastSwitchEnabled: false,
+             flushBufferAtTrackSwitch: false,
+             bufferPruningInterval: 10,
+             bufferToKeep: 20,
+             jumpGaps: true,
+             jumpLargeGaps: true,
+             smallGapLimit: 1.5,
+             stableBufferTime: stableBuffer,
+             bufferTimeAtTopQuality: 30,
+             bufferTimeAtTopQualityLongForm: bufferAtTopQuality,
+             longFormContentDurationThreshold: 600,
+             wallclockTimeUpdateInterval: 50,
+             lowLatencyEnabled: false,
+             keepProtectionMediaKeys: false,
+             useManifestDateHeaderTimeSource: true,
+             useSuggestedPresentationDelay: true,
+             useAppendWindow: true,
+             manifestUpdateRetryInterval: 100,
+             liveCatchUpMinDrift: 0.02,
+             liveCatchUpMaxDrift: 0,
+             liveCatchUpPlaybackRate: 0.5,
+             lastBitrateCachingInfo: {
+                 enabled: true,
+                 ttl: 360000
+             },
+             lastMediaSettingsCachingInfo: {
+                 enabled: true,
+                 ttl: 360000
+             },
+             cacheLoadThresholds: {
+                 video: 50,
+                 audio: 5
+             },
+             retryIntervals: {
+                 MPD: 500,
+                 XLinkExpansion: 500,
+                 InitializationSegment: 1000,
+                 IndexSegment: 1000,
+                 MediaSegment: 1000,
+                 BitstreamSwitchingSegment: 1000,
+                 other: 1000,
+                 lowLatencyReductionFactor: 10
+             },
+             retryAttempts: {
+                 MPD: 3,
+                 XLinkExpansion: 1,
+                 InitializationSegment: 3,
+                 IndexSegment: 3,
+                 MediaSegment: 3,
+                 BitstreamSwitchingSegment: 3,
+                 other: 3,
+                 lowLatencyMultiplyFactor: 5
+             },
+             abr: {
+                 //movingAverageMethod: Constants.MOVING_AVERAGE_SLIDING_WINDOW,
+                 //ABRStrategy: Constants.ABR_STRATEGY_DYNAMIC,
+                 bandwidthSafetyFactor: 0.9,
+                 useDefaultABRRules: true,
+                 useBufferOccupancyABR: false,
+                 useDeadTimeLatency: true,
+                 limitBitrateByPortal: false,
+                 usePixelRatioInLimitBitrateByPortal: false,
+                 maxBitrate: {
+                     audio: -1,
+                     video: maxBitrate
+                 },
+                 minBitrate: {
+                     audio: -1,
+                     video: minBitrate
+                 },
+                 maxRepresentationRatio: {
+                     audio: 1,
+                     video: 1
+                 },
+                 initialBitrate: {
+                     audio: -1,
+                     video: -1
+                 },
+                 initialRepresentationRatio: {
+                     audio: -1,
+                     video: -1
+                 },
+                 autoSwitchBitrate: {
+                     audio: true,
+                     video: true
+                 }
+             },
+             cmcd: {
+                 enabled: false,
+                 sid: null,
+                 cid: null,
+                 did: null
+             }
+         }
+    });
 }
